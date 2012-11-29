4fee9c685b476bf027ff0726e6493c30cd1ce1a2
TJ Holowaychuk <tj@vision-media.ca>
2012-11-29 14:47:01 -0800

    add example

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-module.exports = require('./lib/css-whitespace');
+/**
+ * Module dependencies.
+ */
+
+var lexer = require('./lexer')
+  , parser = require('./parser')
+  , compiler = require('./compiler');
+
+/**
+ * Compile a whitespace significant
+ * `str` of CSS to the valid CSS
+ * equivalent.
+ *
+ * @param {String} str
+ * @return {String}
+ * @api public
+ */
+
+module.exports = function(str){
+  
+};
